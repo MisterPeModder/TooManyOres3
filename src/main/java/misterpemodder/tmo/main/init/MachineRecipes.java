7ef2bf49435a4b80db678678a29279ff406bb612
MisterPeModder <yguaye44@gmail.com>
2017-03-28 20:38:44 +0200

    New Recipe: Magma Cream => Lava + Slime ball

--- src/main/java/misterpemodder/tmo/main/init/MachineRecipes.java
+++ src/main/java/misterpemodder/tmo/main/init/MachineRecipes.java
@@ -21,6 +21,7 @@ public final class MachineRecipes {
 		TooManyOresAPI.INJECTOR_RECIPES.add(new InjectorFillDrainRecipe(TransferMode.EXTRACTION));
 		
 		TooManyOresAPI.registryHandler.registerInjectorRecipeExtraction(TMORefs.PREFIX+"magma_to_lava", new ItemStack(Blocks.MAGMA), new FluidStack(FluidRegistry.LAVA, 100), new ItemStack(Blocks.NETHERRACK), 40);
+		TooManyOresAPI.registryHandler.registerInjectorRecipeExtraction(TMORefs.PREFIX+"magma_cream_to_lava", new ItemStack(Items.MAGMA_CREAM), new FluidStack(FluidRegistry.LAVA, 25), new ItemStack(Items.SLIME_BALL), 40);
 		
 		if(!new ItemStack(Items.GLASS_BOTTLE).hasCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null)) {
 			TooManyOresAPI.registryHandler.registerInjectorRecipeInjection(TMORefs.PREFIX+"fill_water_bottle", new FluidStack(FluidRegistry.WATER, 250), new ItemStack(Items.GLASS_BOTTLE), PotionUtils.addPotionToItemStack(new ItemStack(Items.POTIONITEM), PotionTypes.WATER), 20);
