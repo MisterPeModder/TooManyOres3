f69697838e9d9a20ce309b739307189e71f36bc7
MisterPeModder <yguaye44@gmail.com>
2017-04-01 20:01:16 +0200

    removed unused interface

--- src/main/java/misterpemodder/tmo/main/blocks/properties/ISlabVariant.java
+++
@@ -1,7 +0,0 @@
-package misterpemodder.tmo.main.blocks.properties;
-
-import net.minecraft.block.material.MapColor;
-
-public interface ISlabVariant extends IBlockVariant {
-	public MapColor getMapColor();
-}
