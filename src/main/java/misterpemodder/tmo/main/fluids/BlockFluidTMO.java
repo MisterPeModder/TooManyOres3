30ab2c34ed81bcc9e61ea10140b842d4e4399af1
MisterPeModder <yguaye44@gmail.com>
2017-04-02 04:32:36 +0200

    fluids have now their own damage sources

--- src/main/java/misterpemodder/tmo/main/fluids/BlockFluidTMO.java
+++ src/main/java/misterpemodder/tmo/main/fluids/BlockFluidTMO.java
@@ -1,11 +1,11 @@
 package misterpemodder.tmo.main.fluids;
 
+import misterpemodder.tmo.main.utils.DamageSourceFluid;
 import misterpemodder.tmo.main.utils.TMORefs;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.BlockFluidClassic;
@@ -35,9 +35,8 @@ public class BlockFluidTMO extends BlockFluidClassic {
 	@Override
 	public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
 		super.onEntityCollidedWithBlock(worldIn, pos, state, entityIn);
-		//entityIn.setFire(new Random().nextInt(3)+3);
 		if(entityIn instanceof EntityLivingBase) {
-			entityIn.attackEntityFrom(DamageSource.LAVA, 6F);
+			entityIn.attackEntityFrom(DamageSourceFluid.getDamageSourceForFluid(this.getFluid()), 6F);
 		}
 	}
 
---
+++ src/main/java/misterpemodder/tmo/main/utils/DamageSourceFluid.java
@@ -0,0 +1,52 @@
+package misterpemodder.tmo.main.utils;
+
+import java.util.Random;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.MobEffects;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraftforge.fluids.Fluid;
+
+public class DamageSourceFluid extends DamageSource {
+	
+	public static final DamageSource FROZIUM = new DamageSourceFluid("frozium",4);
+	public static final DamageSource IGNUM = new DamageSourceFluid("ignum",2);
+			
+	private final int messagesNum;
+
+	public DamageSourceFluid(String damageTypeIn, int messagesNum) {
+		super(damageTypeIn);
+		this.setDamageBypassesArmor();
+		this.messagesNum = messagesNum;
+	}
+	
+	@Override
+	public ITextComponent getDeathMessage(EntityLivingBase entity) {
+		String str = "death."+TMORefs.MOD_ID+".fluid."+this.damageType;
+		
+		if(this == IGNUM && entity.getActivePotionEffect(MobEffects.FIRE_RESISTANCE) != null) {
+			str += "."+messagesNum;
+		}
+		else if(this.messagesNum > 0) {
+			str += "."+new Random().nextInt(messagesNum);
+		}
+		
+		return new TextComponentTranslation(str, entity.getDisplayName());
+	}
+	
+	public static DamageSource getDamageSourceForFluid(Fluid fluid) {
+		String name = fluid.getName();
+		switch(fluid.getName()) {
+			case "liquidignum":
+				return IGNUM;
+			case "liquidfrozium":
+				return FROZIUM;
+			default:
+				return DamageSource.LAVA;
+		}
+		
+	}
+
+}
